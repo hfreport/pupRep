0b3e89460d0683cffd669bb0e900f42d4160b93d
fenghu <worldhf@163.com>
2019-10-09 13:47:16 +0800

    提交爬取脚本

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,40 @@
-const globalConfig = {
-  "gotoUrl" : ['https://www.cnki.net'], //需要跳转的url
+/**
+ *  pup配置文件
+ */
+const strUtl  = require("../utils/strUtil");
+const path = require('path');
+const file = require("fs");
 
+// 配置文件，搜索关键词，
+const globalConfig = {
+  host : 'https://zhaoqize.github.io',
+  // "gotoUrl" : ['https://www.cnki.net'], //需要跳转的url
+  gotoUrl : ['{0}/puppeteer-api-zh_CN/'], //需要跳转的url
+
+  basePath : './result/',
+
+  //初始化配置
+  init : function (obj) {
+
+    let _host = this.host, _gotoUrl = this.gotoUrl;
+
+    if (!_host) {
+      throw new TypeError('globalConfig.host is must config');
+    }
+
+    if (!_gotoUrl || _gotoUrl.length === 0) {
+      throw new TypeError('globalConfig.gotoUrl is must config');
+    }
+
+    let config = {
+      inp : obj?obj.inp||[_host]:[_host]
+    };
+
+    this.gotoUrl.replace(config.inp);
+  }
 };
+
+//初始化配置
+globalConfig.init();
+
 exports.globalConfig = globalConfig;
--- src/service/rep/rep.js
+++ src/service/rep/rep.js
@@ -1,55 +1,95 @@
+
 const pup = require("puppeteer");
-const config = require("../../config/config");
 const fileUtil = require("fs");
+const path = require("path");
+
+//获取配置
+const config = require("../../config/config");
 
 (async () => {
     //打开浏览器，支持多种版本，远程的浏览器
     const launch = await pup.launch({
-        // headless : false,
-        // devtools : true
+        headless : false,
+        devtools : true
     });
     //打开窗口
     let page = await launch.newPage();
     //调整窗口大小
-    await page.setViewport({width: 1200, height: 600});
+    await page.setViewport({width: 1200, height: 700});
     // //设置请求拦截
     await page.setRequestInterception(true);
-    //绑定拦截器
-    await page.on('request',(req)=>{
-        //打印页面发出的请求
-        // console.log(req.url());
-        // console.log(req.resourceType());
+
+    let gotoUrl = config.globalConfig.gotoUrl;
+    let basePath = config.globalConfig.basePath;
+
+    //绑定请求拦截器
+    page.on('request',(req)=>{
         req.continue();
     });
-    let gotoUrl = config.globalConfig.gotoUrl;
-    //跳转叶面
+
+    //绑定响应拦截器
+    page.on('response', async (resp) => {
+        let req = resp.request(), url = req.url();
+        //替换保存路径
+        let savePath;
+        if (resp.ok()) {
+            //如果是本次请求
+            if (url === gotoUrl[0]) {
+                savePath = path.join(basePath, 'content.html');
+            } else if (url.startsWith(config.globalConfig.host) && !url.endsWith('/')) {
+                //如果是页面发出的其它请求
+                savePath = url.replace(gotoUrl[0], basePath);
+            }
+            let text = await resp.text();
+            if (url.endsWith('api.md')) {
+                let headers = resp.headers();
+                console.log(headers);
+            }
+
+            //如果保存路径为 undefined
+            if (!savePath) {
+                // console.log(" 跳过的url：  " + url);
+                return;
+            }
+            //保存
+            try {
+                if (!fileUtil.existsSync(savePath)) {
+                    fileUtil.mkdirSync(path.dirname(savePath), {recursive:true});
+                }
+                fileUtil.writeFileSync(savePath, text, {
+                    encoding : 'utf-8'
+                });
+
+            } catch (e) {
+                console.log(e);
+            }
+        }
+    });
+
+    //跳转路径
     let res = await page.goto(gotoUrl[0]);
 
-    await page.waitFor(1000);//等待搜索结果
+    await page.waitFor(30000);//等待搜索结果
 
-    let result = await page.evaluate(()=>{
+    //获取页面中的元素
+    let alis = await page.$$eval('sidebar-component sidebar-item a', (ele) => {
         let data = [];
-        //需要注意页面是否加载完毕
-        //假设网页已经引入了jquery, 如果未引入，需要添加jquery脚本
-        let as = $('.unit ul li a');
-        console.log(as);
-        as.each((index, row) => {
-            let r = $(row),d = {};
-            d['name'] = r.html();
-            d['href'] = r.attr('href');
+        for (const e of ele) {
+            let d = {};
+            d.href = e.href;
+            d.name = e.innerText;
             data.push(d);
-        });
+        }
         return data;
-    }).catch((err)=>{
-        console.log(err);
     });
-    try {
-        fileUtil.writeFile('result.json', JSON.stringify(result), async (err) => {
-            console.log(err);
-        });
-    } catch (e) {
-        console.log(e);
-    }
-    await page.close();
-    await launch.close();
+    console.log(JSON.stringify(alis));
+    // try {
+    //     fileUtil.writeFileSync(path.join(basePath, 'content.html'), await page.content());
+    // } catch (e) {
+    //     console.log(e);
+    // }
+
+
+    // await page.close();
+    // await launch.close();
 })();
